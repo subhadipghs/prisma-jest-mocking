3480ad1497218d75dd0a3f4e3d7deb51a2f5cb6c
Subhadip  Ghosh <ghoshsubhadipghosh@gmail.com>
2022-10-12 10:57:04 +0530

    prettier installed

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
-import { PrismaClient } from "@prisma/client";
-import { getPrimsaClient } from "./prisma/get-primsa-client";
+import { PrismaClient } from "@prisma/client"
+import { getPrimsaClient } from "./prisma/get-primsa-client"
 
-async function main(primsa: PrismaClient): Promise<string[]> {
-  await primsa.$connect();
+async function main(
+  primsa: PrismaClient
+): Promise<string[]> {
+  await primsa.$connect()
   const users = await primsa.user.findMany({
     select: {
       email: true,
     },
-  });
-  await primsa.$disconnect();
-  return users.map((user) => user.email);
+  })
+  await primsa.$disconnect()
+  return users.map(user => user.email)
 }
 
 if (require.main === module) {
-  const primsa = getPrimsaClient();
-  main(primsa).then(console.log).catch(console.error);
+  const primsa = getPrimsaClient()
+  main(primsa).then(console.log).catch(console.error)
 }
 
-export { main };
+export { main }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,30 @@
-import { PrismaClient } from "@prisma/client";
-import { users } from "../fixtures/users.fixture";
-import { getPrimsaClient } from "./get-primsa-client";
+import { PrismaClient } from "@prisma/client"
+import { users } from "../fixtures/users.fixture"
+import { getPrimsaClient } from "./get-primsa-client"
 
 async function init(primsa: PrismaClient): Promise<void> {
   const usersWithoutIds = users.map(({ email, name }) => {
     return {
       name,
       email,
-    };
-  });
+    }
+  })
   await primsa.user.createMany({
     data: usersWithoutIds,
-  });
+  })
 }
 
-(async () => {
-  const prisma = getPrimsaClient();
+; (async () => {
+  const prisma = getPrimsaClient()
   try {
-    console.log("Started seeding... 🌱");
-    await prisma.$connect();
-    await init(prisma);
-    console.log("Done 🌲");
+    console.log("Started seeding... 🌱")
+    await prisma.$connect()
+    await init(prisma)
+    console.log("Done 🌲")
   } catch (e) {
-    console.error(e);
+    console.error(e)
   } finally {
-    await prisma.$disconnect();
-    process.exit();
+    await prisma.$disconnect()
+    process.exit()
   }
-})();
+})()
